ff630d1555b68bb2ae7f56ca11b93766865a725f
NAVNATH SATRE <75266852+navnathsatre@users.noreply.github.com>
2021-07-21 22:31:37 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import time
+
+# with st.empty():
+#     for seconds in range(60):
+#         st.write(f"⏳ {seconds} seconds have passed")
+#         time.sleep(1)
+#     st.write("✔️ 1 minute over!")
+    
+placeholder = st.empty()
+
+# Replace the placeholder with some text:
+placeholder.text("Hello")
+
+# Replace the text with a chart:
+placeholder.line_chart({"data": [1, 5, 2, 6]})
+
+# Replace the chart with several elements:
+with placeholder.beta_container():
+    st.write("This is one element")
+    st.write("This is another")
+
+# Clear all those elements:
+placeholder.empty()
